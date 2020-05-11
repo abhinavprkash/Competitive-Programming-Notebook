d6d8ec319e85749584e6abb6f8bd4d9fe58c0369
Ritik <ritiksingla28@gmail.com>
2020-05-11 15:37:30 +0530

    Basics of lines and planes added

---
+++ Geometry/2d_basics.cpp
@@ -0,0 +1,67 @@
+struct point2d {
+    ld x, y;
+    point2d() {}
+    point2d(ld x, ld y): x(x), y(y) {}
+    point2d& operator+=(const point2d &t) {
+        x += t.x;
+        y += t.y;
+        return *this;
+    }
+    point2d& operator-=(const point2d &t) {
+        x -= t.x;
+        y -= t.y;
+        return *this;
+    }
+    point2d& operator*=(ld t) {
+        x *= t;
+        y *= t;
+        return *this;
+    }
+    point2d& operator/=(ld t) {
+        x /= t;
+        y /= t;
+        return *this;
+    }
+    point2d operator+(const point2d &t) const {
+        return point2d(*this) += t;
+    }
+    point2d operator-(const point2d &t) const {
+        return point2d(*this) -= t;
+    }
+    point2d operator*(ld t) const {
+        return point2d(*this) *= t;
+    }
+    point2d operator/(ld t) const {
+        return point2d(*this) /= t;
+    }
+};
+point2d operator*(ld a, point2d b) {
+    return b * a;
+}
+ld cross(point2d a, point2d b) {
+    return a.x * b.y - a.y * b.x;
+}
+ld dot(point2d a, point2d b) {
+    return a.x * b.x + a.y * b.y;
+}
+// takes (x1,y1),(x2-x1,y2-y1),(x3,y3),(x4-x3,y4-y3)
+point2d intersect(point2d a1, point2d d1, point2d a2, point2d d2) {
+	if(cross(d1,d2)==0)
+		{
+			cout<<"Parallel lines\n";
+			return point2d();
+		}
+    return a1 + cross(a2 - a1, d2) / cross(d1, d2) * d1;
+}
+ld norm(point2d a) {
+    return dot(a, a);
+}
+ld abs(point2d a) {
+    return sqrt(norm(a));
+}
+ld proj(point2d a, point2d b) {
+    return dot(a, b) / abs(b);
+}
+ld angle(point2d a, point2d b) {
+    return acos(dot(a, b) / abs(a) / abs(b));
+}
