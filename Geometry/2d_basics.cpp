281f4cc6e12d22aca3bd934e12cd37c49e2aa5e0
Ritik <44062007+ritiksingla@users.noreply.github.com>
2020-05-11 17:20:40 +0530

    Comments added for intersection of two points

--- Geometry/2d_basics.cpp
+++ Geometry/2d_basics.cpp
@@ -45,6 +45,9 @@ ld dot(point2d a, point2d b) {
     return a.x * b.x + a.y * b.y;
 }
 // takes (x1,y1),(x2-x1,y2-y1),(x3,y3),(x4-x3,y4-y3)
+// Form: r=a1+t*d1 where {a1} is a starting position vector and {d1} is the parallel vector to the given line.
+// r=a1+t*d1 and cross_product((r-a2),d2)=0
+// Therefore, t=a1+(cross_product((a2-a1),d2)/cross_product(d1,d2))*d1
 point2d intersect(point2d a1, point2d d1, point2d a2, point2d d2) {
 	if(cross(d1,d2)==0)
 		{
