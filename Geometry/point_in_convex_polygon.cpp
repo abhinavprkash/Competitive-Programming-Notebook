2aac5df77a3680965fbd490dab994c6a44489da1
Ritik <ritiksingla28@gmail.com>
2020-05-12 00:28:38 +0530

    Check point in convex polygon added

---
+++ Geometry/point_in_convex_polygon.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef tree<int,null_type,less_equal <int>,rb_tree_tag,tree_order_statistics_node_update>ordered_set;
+using ll = long long;
+using ld = long double;
+#define all(x) x.begin(), x.end()
+#define ff first
+#define ss second
+#define rep(i,n) for (ll i = 0; i < n; ++i)
+#define forn(i,a,b) for (ll i =a; i <=b; ++i)
+#define revforn(i,a,b) for (ll i =a; i >=b; --i)
+void debug() {cerr << "\n";}
+template<typename Head, typename... Tail>
+void debug(Head H, Tail... T) {cerr << H << " "; debug(T...);}
+#define debug(...) cerr<<"["<<#__VA_ARGS__<<"] : ";debug(__VA_ARGS__);
+const ll maxn = 1e5+10;
+struct pt{
+	ll x,y;
+};
+//Sort slopes in anticlockwise order
+//(y1-0)/(x1-0)<(y2-0)/(x2-0)
+bool operator<(const pt&a,const pt&b)
+{
+	return (a.y*b.x<b.y*a.x);
+}
+// Checks if slope of line (point->a)>(point->b)
+bool orientation(pt a,pt b,pt point)
+{
+	return (a.y-point.y)*(b.x-point.x)>=(a.x-point.x)*(b.y-point.y);
+}
+void solve()
+{
+	ll n,m,k;
+	cin>>n>>m>>k;
+	vector<pt>p(n);
+	ll id{};
+	rep(i,n)
+	{
+		cin>>p[i].x>>p[i].y;
+		if(p[i].x<p[id].x||(p[i].x==p[id].x && p[i].y<p[id].y))
+			id=i;
+	}
+	pt zero=p[id];
+	rotate(p.begin(),p.begin()+id,p.end());
+	p.erase(p.begin());
+	vector<pt>seq(--n);
+	rep(i,n)
+	{
+		seq[i].x=p[i].x-zero.x;
+		seq[i].y=p[i].y-zero.y;
+	}
+	ll x,y,ans{};
+	rep(i,m)
+	{
+		bool flag=false;
+		pt point,q;
+		cin>>point.x>>point.y;
+		q.x=point.x;q.y=point.y;
+		if(point.x>zero.x||(point.x==zero.x&&point.y>=zero.y))
+		{
+			point.x=point.x-zero.x,point.y=point.y-zero.y;
+			ll idx=upper_bound(seq.begin(),seq.end(),point)-seq.begin();
+			if(idx==n)
+			{
+				// Same slope, i.e. collinear on y direction
+				// point.y/point.x==seq[i].y/seq[i].x
+				if(point.y*seq[n-1].x==point.x*seq[n-1].y)
+					idx--;
+			}
+			if(idx && idx!=n)
+			{
+				if(orientation(p[idx],p[idx-1],q))
+					flag=true;
+			}
+		}
+		ans+=flag;
+	}
+	if(ans>=k)
+		cout<<"YES\n";
+	else
+		cout<<"NO\n";
+}
+int32_t main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	ll t=1;
+	rep(i, t)
+	{
+		solve();
+	}
+	return 0;
+}
+// https://codeforces.com/problemsets/acmsguru/problem/99999/253
+// https://codeforces.com/problemsets/acmsguru/submission/99999/79775844
